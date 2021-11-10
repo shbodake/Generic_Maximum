67153c69da9c723b0d4de7fff5c9ccc1da0b80f4
shbodake <sayalibodake28@gmail.com>
2021-11-10 23:19:03 +0530

    Added: By using compareTo find max of three integer

--- src/test/java/com/blp/generic/MaximumTest.java
+++ src/test/java/com/blp/generic/MaximumTest.java
@@ -11,4 +11,16 @@ public class MaximumTest {
         int result = Maximum.findMaximum(7,5,2);
         Assert.assertEquals(7, result );
     }
+
+    @Test
+    public void integerMaxAtSecondPosition() {
+        int result = Maximum.findMaximum(7,5,2);
+        Assert.assertEquals(5, result );
+    }
+
+    @Test
+    public void integerMaxAtThirdPosition() {
+        int result = Maximum.findMaximum(7,5,2);
+        Assert.assertEquals(2, result );
+    }
 }
