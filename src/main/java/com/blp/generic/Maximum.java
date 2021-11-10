57b1b00c5b8bdd83643fb1759f0a6930ee717eee
shbodake <sayalibodake28@gmail.com>
2021-11-10 23:48:15 +0530

    Added: By using compareTo find max of three float values

--- src/main/java/com/blp/generic/Maximum.java
+++ src/main/java/com/blp/generic/Maximum.java
@@ -1,8 +1,8 @@
 package com.blp.generic;
 
 public class Maximum {
-    public static Integer findMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public static Float findMaximum(Float a, Float b, Float c) {
+        Float max = a;
         if (b.compareTo(a) > 0 && b.compareTo(c) > 0) {
             max = b;
         }
@@ -13,6 +13,6 @@ public class Maximum {
     }
         public static void main (String[]args){
             System.out.println("Welcome to Generics Maximum Program");
-              Maximum.findMaximum(2,5,7)  ;
+
         }
     }
